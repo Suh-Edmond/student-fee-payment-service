7a12eec8c92a11171fea68e1135a204be34111e3
Suh-Edmond <suhedmond25@yahoo.com>
2026-03-16 06:29:03 +0100

    fix: add a global Cor configurer

--- src/main/java/com/student_mgt_platform/fee_payment/config/WebConfig.java
+++ src/main/java/com/student_mgt_platform/fee_payment/config/WebConfig.java
@@ -6,16 +6,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig  {
+public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins("*") // Your frontend URL
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("Origin", "Content-Type", "Accept") // Explicit
+                        .allowCredentials(true);
+                ;
             }
         };
     }
