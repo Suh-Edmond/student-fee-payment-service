d59777c10e7dd60bf4f2a630c2ed5848af3a6a34
Suh-Edmond <suhedmond25@yahoo.com>
2026-03-16 22:16:51 +0100

    add size validation

--- src/main/java/com/student_mgt_platform/fee_payment/dto/StudentAccountRequestDto.java
+++ src/main/java/com/student_mgt_platform/fee_payment/dto/StudentAccountRequestDto.java
@@ -1,14 +1,17 @@
 package com.student_mgt_platform.fee_payment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class StudentAccountRequestDto {
     @NotBlank(message = "studentNumber is required")
+    @Size(min = 5, max = 10, message = "studentNumber must have at least 5 characters and should not be more than 10 characters")
     private String studentNumber;
 
     @NotBlank(message = "studentName is required")
+    @Size(min = 3, max = 255, message = "studentName must have at least 3 characters and should not be more than 255 characters")
     private String studentName;
 
     @NotBlank(message = "institutionFee is required")
